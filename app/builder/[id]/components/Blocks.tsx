1022bf018fe05deeb9e62bf86d6085e8df779d72
Eugen Guriev <gurievcreative@gmail.com>
2024-04-30 20:50:16 +0300

    chore: clean up

--- app/builder/[id]/components/Blocks.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import type { ReactNode } from "react";
-
-const Blocks = ({ children }: { children: ReactNode }) => {
-  return <div className="flex w-full flex-col">{children}</div>;
-};
-
-export default Blocks;
--- app/strm/[id]/page.tsx
+++
@@ -1,8 +0,0 @@
-import getStream from "@/app/actions/getStream";
-
-const StreamPage = async ({ params: { id } }: { params: { id: string } }) => {
-  const stream = await getStream(id);
-  return <div>{JSON.stringify(stream)}</div>;
-};
-
-export default StreamPage;
