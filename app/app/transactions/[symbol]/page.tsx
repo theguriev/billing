376f0beeb60ad505c70008a6fbd750f906c3f66e
Eugen Guriev <gurievcreative@gmail.com>
2024-05-08 22:53:56 +0300

    refactor: transactions page and add related components in app/app/transactions/[symbol]/

---
+++ app/app/transactions/[symbol]/page.tsx
@@ -0,0 +1,48 @@
+import { Suspense } from "react";
+
+import Link from "next/link";
+
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+
+import TransactionsTable from "./components/TransactionsTable";
+
+const AddTokenPage = async ({
+  params: { symbol },
+}: {
+  params: { symbol: string };
+}) => {
+  return (
+    <>
+      <Breadcrumb className="hidden md:flex">
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link href="/app">Tokens</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>
+              {symbol.toUpperCase()} - transactions
+            </BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+      <div className="flex items-center">
+        <h1 className="text-lg font-semibold md:text-2xl">Transactions</h1>
+      </div>
+      <Suspense fallback={null}>
+        <TransactionsTable symbol={symbol} />
+      </Suspense>
+    </>
+  );
+};
+
+export default AddTokenPage;
