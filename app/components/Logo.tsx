bdb321aad17c4431db1c9529405ac8a2d00b7052
Eugen Guriev <gurievcreative@gmail.com>
2024-05-17 22:37:48 +0300

    fix: stroke-width attribute in Logo.tsx

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -13,42 +13,42 @@ const Logo = (props: SVGProps<SVGSVGElement>) => (
       r="23"
       fill="#FFFBFB"
       stroke="black"
-      stroke-width="2"
+      strokeWidth="2"
     />
     <path
       d="M72 62C72 75.2548 82.7452 86 96 86C109.255 86 120 75.2548 120 62C120 48.7452 109.255 38 96 38"
       stroke="black"
-      stroke-width="2"
+      strokeWidth="2"
       fill="transparent"
     />
-    <line x1="1" y1="2" x2="1" y2="86" stroke="black" stroke-width="2" />
-    <line x1="64" y1="1" x2="64" y2="85" stroke="black" stroke-width="2" />
-    <line x1="56" y1="1" x2="56" y2="85" stroke="black" stroke-width="2" />
+    <line x1="1" y1="2" x2="1" y2="86" stroke="black" strokeWidth="2" />
+    <line x1="64" y1="1" x2="64" y2="85" stroke="black" strokeWidth="2" />
+    <line x1="56" y1="1" x2="56" y2="85" stroke="black" strokeWidth="2" />
     <path
       d="M119.319 76.7251C113.616 83.0617 128.418 83.5774 125.463 78.2116C123.97 75.4995 117.654 74.6889 118.873 78.9549"
       stroke="#FFE600"
-      stroke-width="2"
+      strokeWidth="2"
       stroke-linecap="round"
       fill="transparent"
     />
     <path
       d="M132.698 70.0355C132.497 73.0484 131.673 76.0068 131.186 78.9797C130.445 83.5114 132.932 77.5695 133.986 76.18C134.507 75.4939 137.38 70.9448 138.619 71.2C140.489 71.585 137.732 77.3313 140.552 75.8084C145.055 73.3767 145.486 79.9871 144.293 82.9687"
       stroke="#FFE600"
-      stroke-width="2"
+      strokeWidth="2"
       stroke-linecap="round"
       fill="transparent"
     />
     <path
       d="M148.307 79.8469C152.013 79.0747 154.779 78.0932 155.69 74.0493C156.647 69.8015 152.109 74.0695 151.255 74.9412C148.892 77.3546 147.247 80.4628 147.093 83.935C146.917 87.8952 155.561 85.4564 157.672 84.7526"
       stroke="#FFE600"
-      stroke-width="2"
+      strokeWidth="2"
       stroke-linecap="round"
       fill="transparent"
     />
     <path
       d="M107.813 5.86418C106.434 3.74348 103.866 2.85305 101.463 2.45246C91.4993 0.791908 81.7796 3.54809 77.5058 13.2853C73.4313 22.5684 74.004 32.2635 81.6148 39.4335C86.3147 43.8611 92.344 46.9891 97.752 50.4157C102.526 53.4405 106.829 56.8277 111.399 60.103"
       stroke="#FFE600"
-      stroke-width="3"
+      strokeWidth="3"
       stroke-linecap="round"
       fill="transparent"
     />
