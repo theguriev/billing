47098dce49a2ef26c612a16073a5ca6d4334a15c
Eugen Guriev <gurievcreative@gmail.com>
2024-05-24 20:19:21 +0300

    refactor: update middleware to handle unauthorized access to /app route

--- middleware.ts
+++ middleware.ts
@@ -2,13 +2,6 @@ import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname.startsWith("/app")) {
-    const privateKey = request.cookies.get("privateKey")?.value;
-    if (!privateKey) {
-      return NextResponse.redirect(new URL("/wallet", request.url));
-    }
-  }
-
   if (request.nextUrl.pathname.startsWith("/blls")) {
     const privateKey = request.cookies.get("privateKey")?.value;
     if (!privateKey) {
