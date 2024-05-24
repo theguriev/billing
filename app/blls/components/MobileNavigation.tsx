6211414cbccde8fb1fd50fc065f40bcdfadacdd7
Eugen Guriev <gurievcreative@gmail.com>
2024-05-24 23:18:11 +0300

    refactor: update MobileNavigation component to use fixed positioning

--- app/blls/components/MobileNavigation.tsx
+++ app/blls/components/MobileNavigation.tsx
@@ -10,13 +10,13 @@ import links from "../constants/links";
 const MobileNavigation = () => {
   const pathname = usePathname();
   return (
-    <nav className="grid gap-2 text-lg font-medium">
+    <nav className="fixed inset-x-0 bottom-0 flex justify-between border-t bg-white px-4 py-2 md:hidden">
       {links.map((link) => (
         <Link
           key={link.text}
           href={link.href}
           className={cn(
-            "mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground",
+            "mx-[-0.65rem] flex flex-col items-center gap-2 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground",
             link.isActive(pathname) && "bg-muted"
           )}
         >
