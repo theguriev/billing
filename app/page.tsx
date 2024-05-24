55fa47bc01cbb270c3ce0cfbdf971ea44a2ee3df
Eugen Guriev <gurievcreative@gmail.com>
2024-05-24 22:34:59 +0300

    refactor: update Logo component to use consistent size and replace BitcoinIcon

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 /* eslint-disable tailwindcss/no-arbitrary-value */
 import Link from "next/link";
 
+import Logo from "./components/Logo";
+
 export default function Component() {
   return (
     <div className="flex min-h-dvh flex-col">
       <header className="flex h-14 items-center px-4 lg:px-6">
         <Link className="flex items-center justify-center" href="#">
-          <BitcoinIcon className="size-6" />
+          <Logo className="size-14" />
           <span className="sr-only">Crypto Creator</span>
         </Link>
         <nav className="ml-auto flex gap-4 sm:gap-6">
@@ -244,24 +246,5 @@ export default function Component() {
         </p>
       </footer>
     </div>
-  );
-}
-
-function BitcoinIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M11.767 19.089c4.924.868 6.14-6.025 1.216-6.894m-1.216 6.894L5.86 18.047m5.908 1.042-.347 1.97m1.563-8.864c4.924.869 6.14-6.025 1.215-6.893m-1.215 6.893-3.94-.694m5.155-6.2L8.29 4.26m5.908 1.042.348-1.97M7.48 20.364l3.126-17.727" />
-    </svg>
   );
 }
