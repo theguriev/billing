30eda34d13b1c038a3b584b066114ef255b667f6
Eugen Guriev <gurievcreative@gmail.com>
2024-05-24 21:32:39 +0300

    refactor: update getTokens API endpoint to use consistent naming for tokens path

--- app/blls/components/Token.tsx
+++ app/blls/components/Token.tsx
@@ -4,6 +4,7 @@ import relativeTime from "dayjs/plugin/relativeTime";
 import { CalendarClockIcon, HashIcon, WalletIcon } from "lucide-react";
 import Link from "next/link";
 
+import CryptoGradient from "@/app/components/CryptoGradient";
 import { Button } from "@/components/ui/button";
 import {
   Tooltip,
@@ -24,44 +25,37 @@ const Token = ({
 }: components["schemas"]["TokenResponse"] & { canIssue?: boolean }) => {
   return (
     <div className="flex items-center justify-between border-x border-b p-4 first:rounded-t-md first:border-t last:rounded-b-md">
-      <div className="flex flex-col">
+      <div className="flex items-center space-x-2">
+        <CryptoGradient className="size-10 rounded-full" address={address!} />
+
         <div>
-          <Button variant="link" asChild className="p-0">
-            <Link href={`/app/transactions/${symbol}`}>{name}</Link>
-          </Button>
-        </div>
-        <div className="flex flex-wrap items-center space-x-2">
-          <div className="flex items-center">
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <HashIcon className="size-[1em] text-xs text-muted-foreground" />
-              </TooltipTrigger>
-              <TooltipContent>ID: {_id}</TooltipContent>
-            </Tooltip>
-            <div className="text-xs font-medium text-muted-foreground">
-              {symbol?.toUpperCase()}
-            </div>
+          <div>
+            <Button variant="link" asChild className="h-auto p-0">
+              <Link href={`/app/transactions/${symbol}`}>{name}</Link>
+            </Button>
           </div>
-          <div className="flex items-center">
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <CalendarClockIcon className="mr-1 size-[1em] text-xs text-muted-foreground" />
-              </TooltipTrigger>
-              <TooltipContent>Timestamp: {timestamp}</TooltipContent>
-            </Tooltip>
-            <div className="text-xs font-medium text-muted-foreground">
-              created {dayjs(dayjs(timestamp)).fromNow(true)} ago
+          <div className="flex flex-wrap items-center space-x-2">
+            <div className="flex items-center">
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <HashIcon className="size-[1em] text-xs text-muted-foreground" />
+                </TooltipTrigger>
+                <TooltipContent>ID: {_id}</TooltipContent>
+              </Tooltip>
+              <div className="text-xs font-medium text-muted-foreground">
+                {symbol?.toUpperCase()}
+              </div>
             </div>
-          </div>
-          <div className="flex items-center">
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <WalletIcon className="mr-1 size-[1em] text-xs text-muted-foreground" />
-              </TooltipTrigger>
-              <TooltipContent>Owner wallet address</TooltipContent>
-            </Tooltip>
-            <div className="text-xs font-medium text-muted-foreground">
-              {address}
+            <div className="flex items-center">
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <CalendarClockIcon className="mr-1 size-[1em] text-xs text-muted-foreground" />
+                </TooltipTrigger>
+                <TooltipContent>Timestamp: {timestamp}</TooltipContent>
+              </Tooltip>
+              <div className="text-xs font-medium text-muted-foreground">
+                created {dayjs(dayjs(timestamp)).fromNow(true)} ago
+              </div>
             </div>
           </div>
         </div>
