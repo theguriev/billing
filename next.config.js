95fd659b5341e2805907ba09d60300d369b1adac
Eugen Guriev <gurievcreative@gmail.com>
2024-08-10 15:17:42 +0300

    chore: experiments

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
-const { createContentlayerPlugin } = require("next-contentlayer");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
   poweredByHeader: false,
 };
 
-const withContentlayer = createContentlayerPlugin({
-  // Additional Contentlayer config options
-});
-
-module.exports = withContentlayer(nextConfig);
+module.exports = nextConfig;
