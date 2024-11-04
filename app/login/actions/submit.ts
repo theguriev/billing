88a603f052a258f6bedb5bd2d15c5b3909dd552e
Eugen Guriev <gurievcreative@gmail.com>
2024-11-04 19:29:19 +0200

    refactor: update cookie settings to include domain and expiration

--- app/login/actions/submit.ts
+++ app/login/actions/submit.ts
@@ -5,10 +5,20 @@ import { redirect } from "next/navigation";
 
 import type { FormSchema } from "../zod";
 
+const ONE_YEAR = 1000 * 60 * 60 * 24 * 365;
+
 const submit = async (body: FormSchema) => {
   const wallet = Wallet.fromPhrase(body.mnemonic);
-  cookies().set("privateKey", wallet.privateKey, { path: "/" });
-  cookies().set("address", wallet.address, { path: "/" });
+  cookies().set("privateKey", wallet.privateKey, {
+    path: "/",
+    expires: Date.now() + ONE_YEAR,
+    domain: process.env.COOKIE_DOMAIN,
+  });
+  cookies().set("address", wallet.address, {
+    path: "/",
+    expires: Date.now() + ONE_YEAR,
+    domain: process.env.COOKIE_DOMAIN,
+  });
   redirect("/blls");
 };
 
